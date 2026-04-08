e2c68a02a32d099bce56cf24261e982f73efd9f3
srturjoy <srturjoy@gmail.com>
2026-04-08 22:15:48 +0600

    Delete portfolio/portfolio directory

--- portfolio/portfolio/src/hooks/useScrollAnimation.ts
+++
@@ -1,9 +0,0 @@
-import { useInView } from 'framer-motion';
-import { useRef } from 'react';
-
-export const useScrollAnimation = (once = true, margin = "-100px") => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once, margin: margin as any });
-
-  return { ref, isInView };
-};
